12f3be8c4625093380cd43039fb523679562eab2
spencel <spence.lank.sd@gmail.com>
2017-11-08 10:03:28 -0800

    m

--- src/user-interface/RctHeader.jsx
+++ src/user-interface/RctHeader.jsx
@@ -8,11 +8,12 @@ class RctHeader extends React.Component {
   render() {
 		return (
       <div className="RctHeader">
+        <div className="demo">demo</div>
       	<div 
       		id="toggleMenuButton"
       		className="button"
       		onClick={ this.props.rctOnInput.bind( this, "toggleMenuButton" ) }>
-      		<img src={ toggleMenuIcon } width="18" height="18"/>
+      		<img src={ toggleMenuIcon } width="20" height="20"/>
       	</div>
       </div>
     );
