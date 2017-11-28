bcfa8aaf4d5725ad2f4eebf7194aa88c2e126c3b
spencel <spence.lank@gmail.com>
2017-11-27 23:03:03 -0800

    chem

---
+++ src/chemistry/RctBonding.jsx
@@ -0,0 +1,8 @@
+covalent bond
+ionic bond
+hydrogen bond
+van der Waals interaction
+dipole dipole interaction
+ion-induced dipole interaction
+dipole-induced dipole interaction
+dispersion (London forces)
